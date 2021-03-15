5952fba13095b0f0eb065d166f354f844a1359c3
github_username <hamidreza.azarbad77@gmail.com>
2021-03-15 11:08:37 +0330

    add models for djanog admin

--- Consultant/User/admin.py
+++ Consultant/User/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['username', 'avatar', 'first_name', 'last_name', 'phone_number']
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
+
+
+class LawyerAdmin(admin.ModelAdmin):
+    list_display = ['username', 'avatar', 'first_name', 'last_name', 'phone_number', 'accepted']
+
+
+admin.site.register(Lawyer, LawyerAdmin)
