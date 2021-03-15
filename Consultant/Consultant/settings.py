a8bffdcd36822938f922350fac99cde702ac53b6
mojtaba <mojtabanafez96@gmail.com>
2021-03-15 21:44:50 +0330

    add swagger to represent API's

--- Consultant/Consultant/settings.py
+++ Consultant/Consultant/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_swagger',
     'User',
 ]
+REST_FRAMEWORK = { 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema' }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- Consultant/Consultant/urls.py
+++ Consultant/Consultant/urls.py
@@ -16,9 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import url, include
 from django.urls import path
+from django.conf.urls import url
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Pastebin API')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('User.user_urls')),
     path('consultant/', include('User.consultant_urls')),
+    
+    url(r'^$', schema_view),
+
 ]
