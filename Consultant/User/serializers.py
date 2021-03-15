e509b5dbd0585d2cfe866fdc3f35b4fcd692091c
github_username <hamidreza.azarbad77@gmail.com>
2021-03-15 18:27:51 +0330

    write user signup api

--- Consultant/User/serializers.py
+++ Consultant/User/serializers.py
@@ -4,18 +4,19 @@ from rest_framework import serializers
 from .models import *
 
 
-class UserSignupSerializer(serializers.Serializer, ABC):
+class UserSignupSerializer(serializers.Serializer):
     username = serializers.CharField(required=True, allow_null=False, allow_blank=False, max_length=128)
     email = serializers.EmailField(required=True, allow_blank=False, allow_null=False)
     first_name = serializers.CharField(required=True, allow_null=False, allow_blank=False)
     last_name = serializers.CharField(required=True, allow_null=False, allow_blank=False)
-    phone_number = serializers.CharField(required=True, allow_blank=False, allow_null=False, max_length=11)
+    phone_number = serializers.CharField(required=True, allow_blank=False, allow_null=False)
     password = serializers.CharField(required=True, allow_null=False, allow_blank=False, min_length=6, max_length=25)
     password_repetition = serializers.CharField(required=True, allow_null=False, allow_blank=False, min_length=6,
                                                 max_length=25)
-    is_private = serializers.BooleanField(default=False, allow_null=False)
+    private_profile = serializers.BooleanField(default=False, allow_null=False)
 
     def create(self, validated_data):
+        del validated_data['password_repetition']
         return UserProfile.objects.create(**validated_data)
 
     def validate_password(self, password):
@@ -25,22 +26,25 @@ class UserSignupSerializer(serializers.Serializer, ABC):
         # TODO WRITE PASSWORD VALIDATION
         return password
 
-    def validate_password_repetition(self, password_repetition):
+    def validate(self, data):
         """
         Check the password regex.
         """
-        # TODO WRITE PASSWORD_REPETITION VALIDATION
-        return password_repetition
+        if data["password_repetition"] != data["password"]:
+            raise serializers.ValidationError("Repetition of password is not same with password!")
+        return data
 
     def validate_phone_number(self, phone_number):
         """
         Check the phone_number regex.
         """
-        # TODO WRITE PHONE_NUMBER VALIDATION
+        import re
+        if len(phone_number) != 11 or re.search(r"09[0-9]{9}", phone_number) is None:
+            raise serializers.ValidationError("Format of phone_number is not true")
         return phone_number
 
 
-class LawyerSignupSerializer(UserSignupSerializer, ABC):
+class LawyerSignupSerializer(UserSignupSerializer):
     certificate = serializers.FileField(required=True, allow_null=False, allow_empty_file=False)
 
     def create(self, validated_data):
--- Consultant/User/views.py
+++ Consultant/User/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from . import serializers
 
 
 class UserLoginAPI(APIView):
-    def get(self, request, format=None):
-        return Response({'data': 'nothing in user'}, status=status.HTTP_200_OK)
+    def post(self, request, format=None):
+        try:
+            serializer = serializers.UserSignupSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({}, status=status.HTTP_200_OK)
+            else:
+
+                return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response({'status': 'Internal server error!!!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class ConsultantLoginAPI(APIView):
