a7075531e2d551fb5cd70f8a88a1508eda993ff1
mojtaba <mojtabanafez96@gmail.com>
2021-03-17 01:13:08 +0330

    try to resolve heroku error to deployment 5 => not supported sqlite

--- Consultant/settings.py
+++ Consultant/settings.py
@@ -77,13 +77,17 @@ WSGI_APPLICATION = 'Consultant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# ie if Heroku server
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Consultant
+
+
+error: 
+    OperationalError at /admin/login/
+
+
+You can use postgresql:
+
+In settings.py add(at the end of file):
+
+# ie if Heroku server
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+In requirements.txt add:
+
+dj-database-url 
+psycopg2
+Now you can run: heroku run python manage.py migrate
