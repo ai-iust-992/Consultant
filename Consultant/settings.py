8ea6ba2107422c3129915355f4e817f6f0bbd235
github_username <hamidreza.azarbad77@gmail.com>
2021-03-29 11:49:53 +0430

    write swagger

--- Consultant/settings.py
+++ Consultant/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_swagger',
     'corsheaders',
     'User',
 ]
@@ -142,4 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
--- Consultant/urls.py
+++ Consultant/urls.py
@@ -16,19 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import url, include
 from django.urls import path
-from django.conf.urls import url
+from User import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title='Pargar API')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('User.user_urls')),
     path('consultant/', include('User.consultant_urls')),
-    
-    path('swagger/', schema_view),
+    path('swagger/', views.SwaggerUI.as_view(), name='swagger-ui')
 
 ]
 urlpatterns += staticfiles_urlpatterns()
