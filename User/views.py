0fa0d6ab9ca10c929c8143a9fb93685142a1a3c1
mojtaba <mojtabanafez96@gmail.com>
2021-03-29 12:37:26 +0430

    add token authentication

--- User/views.py
+++ User/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from rest_framework.schemas import AutoSchema
 from rest_framework.views import APIView
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 from . import serializers
-
+from django.views.generic import TemplateView
 
 from .models import *
 
@@ -14,23 +16,26 @@ class SwaggerUI(TemplateView):
         return render(request, 'swagger-ui.html')
 
 
-class UserSignupAPI(APIView):
-
+class UserSignupAPI(ObtainAuthToken):
+   
     def post(self, request, format=None):
         try:
             serializer = serializers.UserSignupSerializer(data=request.data)
             if serializer.is_valid():
-                serializer.save()
+                user = serializer.save()
                 return_data = serializers.UserConsultantSerializerReturnData(data=serializer.validated_data)
                 return_data.is_valid(raise_exception=True)
-                return Response(data=return_data.validated_data, status=status.HTTP_200_OK)
+                token, created = Token.objects.get_or_create(user=user)
+                return Response({
+                        'token': token.key,
+                        'data':return_data.data,
+                    }, status=status.HTTP_200_OK)
             else:
                 return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as server_error:
             return Response(server_error.__str__(), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-
-class UserConsultantLoginAPI(APIView):
+class UserConsultantLoginAPI(ObtainAuthToken):
     def post(self, request):
         try:
             serializer = serializers.UserConsultantLoginSerializer(data=request.data)
@@ -41,18 +46,22 @@ class UserConsultantLoginAPI(APIView):
                     user = UserProfile.objects.filter(email=serializer.validated_data['email_username'])
                 if len(user) == 0:
                     return Response({'error': 'This user not found'}, status=status.HTTP_400_BAD_REQUEST)
-                user = user[0]
-                if user.password != serializer.validated_data['password']:
+                if user[0].password != serializer.validated_data['password']:
                     return Response({'error': 'The password is not true'}, status=status.HTTP_400_BAD_REQUEST)
-                return_data = serializers.UserConsultantSerializerReturnData(user, many=False, partial=True)
-                return Response(data=return_data.data, status=status.HTTP_200_OK)
+                return_data = serializers.UserConsultantSerializerReturnData(user[0], many=False, partial=True)
+                
+                token, created = Token.objects.get_or_create(user=user[0])
+                return Response({
+                        'token': token.key,
+                        'data':return_data.data,
+                    }, status=status.HTTP_200_OK)
             else:
                 return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as server_error:
             return Response(server_error.__str__(), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-class ConsultantSignupAPI(APIView):
+class ConsultantSignupAPI(ObtainAuthToken):
 
     def post(self, request, format=None):
         try:
@@ -61,10 +70,15 @@ class ConsultantSignupAPI(APIView):
             else:
                 return Response({'error': 'Type of consultant is not valid!!'}, status=status.HTTP_400_BAD_REQUEST)
             if serializer.is_valid():
-                serializer.save()
+                user = serializer.save()
                 return_data = serializers.UserConsultantSerializerReturnData(data=serializer.validated_data)
                 return_data.is_valid(raise_exception=True)
-                return Response(data=return_data.validated_data, status=status.HTTP_200_OK)
+
+                token, created = Token.objects.get_or_create(user=user)
+                return Response({
+                        'token': token.key,
+                        'data':return_data.data,
+                    }, status=status.HTTP_200_OK)
             else:
                 return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as server_error:
