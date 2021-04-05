291ebde76316319706bced9732b66556609c6cd7
mojtaba <mojtabanafez96@gmail.com>
2021-04-05 12:33:06 +0430

    make channels model

--- channel/models.py
+++ channel/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from User.models import ConsultantProfile
+from User.models import UserProfile
+from django.utils import timezone
+
+
 class channel(models.Model):
-    consultant = models.OneToOneField(ConsultantProfile, verbose_name="channal owner", on_delete=models.CASCADE) 
+    subscribers = models.ManyToManyField( UserProfile, verbose_name="", through='Subscribtion')
+    consultant = models.OneToOneField(ConsultantProfile, verbose_name="channal owner") 
     name = models.CharField(max_length=50)
     description = models.CharField( max_length=500, blank=True)
     invite_link = models.CharField( max_length=250)
+
+class Subscribtion(models.Model):
+    channel = models.ForeignKey( channel, verbose_name="", on_delete=models.CASCADE)
+    user =  models.ForeignKey( UserProfile, verbose_name="", on_delete=models.CASCADE)
+    date_joined = models.DateTimeField( default = timezone.now)
