f2808a5e1fdeb732b6f10556647fefda0c351591
github_username <hamidreza.azarbad77@gmail.com>
2021-04-19 14:10:17 +0430

    write mahdi api!!

--- User/user_urls.py
+++ User/user_urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from  channel.views import UserChannelsAPI
+from  channel.views import UserChannelsAPI, UserRoleInChannelAPI
 
 urlpatterns = [
     path('signup/', views.UserSignupAPI.as_view(), name='user_signup_api'),
     path('login/', views.UserConsultantLoginAPI.as_view(), name='user_login_api'),
     path('channels/', UserChannelsAPI.as_view(), name='user_channels'),
+    path('channels/role/<int:channelId>/', UserRoleInChannelAPI.as_view(), name='user_channels'),
 ]
--- channel/views.py
+++ channel/views.py
@@ -37,7 +37,7 @@ class ChannelAPI(APIView):
 
     def get(self, request, channelId, format=None):
         try:
-            channel = Channel.objects.filter(id = channelId).select_related('consultant')
+            channel = Channel.objects.filter(id=channelId).select_related('consultant')
             if len(channel) == 0:
                 return Response({"error": "This channel id is not exits"}, status=status.HTTP_400_BAD_REQUEST)
             return Response(ChannelSerializer(channel[0]).data, status=status.HTTP_200_OK)
@@ -105,6 +105,31 @@ class UserChannelsAPI(APIView):
                     }
                 ]
             return Response(user_channels, status=status.HTTP_200_OK)
+        except Exception as server_error:
+            return Response(server_error.__str__(), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class UserRoleInChannelAPI(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, channelId, format=None):
+        try:
+            channel = Channel.objects.filter(id=channelId).select_related('consultant__my_secretaries')
+            if len(channel) == 0:
+                return Response({"error": "This channel id is not exits"}, status=status.HTTP_400_BAD_REQUEST)
+            if channel[0].consultant.id == request.user.id:
+                return Response(data={"role": "consultant", "invite_link": channel[0].invite_link},
+                                status=status.HTTP_200_OK)
+            for secretary in channel[0].consultant.my_secretaries.all():
+                if request.user.id == secretary.id:
+                    return Response(data={"role": "secretary", "invite_link": channel[0].invite_link},
+                                    status=status.HTTP_200_OK)
+            for subscriber in channel[0].subscribers.all():
+                if request.user.id == subscriber.id:
+                    return Response(data={"role": "subscriber", "invite_link": channel[0].invite_link},
+                                    status=status.HTTP_200_OK)
+            return Response(data={"role": "nothing", "invite_link": channel[0].invite_link},
+                            status=status.HTTP_200_OK)
         except Exception as server_error:
             return Response(server_error.__str__(), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
